3b250dba428a320e6dba24f2762f33838ad377d2
antonic901 <nikola.antonic@valcon.com>
2025-02-13 15:51:03 +0100

    [Auth] changed: allow only authenticated users to access our resources

--- src/router.py
+++ src/router.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from src.auth.dependencies import validate_token
 from src.item.router import router as item_router
 
-router = APIRouter()
+router = APIRouter(dependencies=[Depends(validate_token)])
 
 router.include_router(item_router, prefix="/items", tags=["items"])
